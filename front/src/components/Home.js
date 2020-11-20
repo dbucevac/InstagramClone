c5e1b1edb7364bacf430e27058ff9b9610bd573c
Darko B <darkobuchevac@gmail.com>
2020-11-20 23:49:56 +0100

    visual aspect for not found and home component

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -10,6 +10,7 @@ class Home extends React.Component {
     this.state = { 
       username: this.props.username,
       loggedInUser: {},
+      followings:[],
       postsOfFollowingUsers:[],
       usersWithPosts:[],
       postsOfFollowingUsersWithImage:[]
@@ -25,11 +26,22 @@ getLoggedInUser(){
     Axios.get('/users/?username='+this.state.username)
         .then(res => {
             this.setState({loggedInUser: res.data[0]});
-            this.getFollowingUsersPosts()
+            this.getFollowings()
         })
         .catch(error => {
             console.log(error)
         })
+}
+
+getFollowings(){
+  Axios.get('/users/'+this.state.loggedInUser.id + '/followings')
+      .then(res => {
+          this.setState({followings: res.data})
+          this.getFollowingUsersPosts()
+      })
+      .catch(error => {
+          console.log(error)
+      })
 }
 
 getFollowingUsersPosts() {
@@ -86,24 +98,38 @@ getFollowingUsersPostsWithImage(){
 }
 
   render() {
-
-    return(     
-      <div className="container">
-      <div className="row">
-        <div className="col s12">  
-       {this.state.postsOfFollowingUsersWithImage.map(post => (
-                <PostItem key={post.postId} userId={post.userId} postId={post.postId} image={post.imgUrl}
-                />
-            ))}
+    if(this.state.followings.length>0){
+      return(     
+        <div className="container">
+        <div className="row">
+          <div className="col s12">  
+         {this.state.postsOfFollowingUsersWithImage.map(post => (
+                  <PostItem key={post.postId} userId={post.userId} postId={post.postId} image={post.imgUrl}
+                  />
+              ))}
+          </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col s12">
+      )
+    }else{
+      return(
+        <div class="valign-wrapper" style={{"width":"100%", "height":"50%", "position": "absolute"}}>
+        <div className="valign" style={{"width":"100%"}}>
+        <div className="container">
+        <div className="row">
+          <div className="col s2"></div>
+          <div className="col s8">  
+            <h4 className="center">Start following InstaClone members today</h4>
+            <h5 className="center">Here are some suggestions</h5>
+          </div>
+          <div className="col s2"></div>
         </div>
       </div>
-        
       </div>
-    )
+      </div>
+      )
+    }
+    
   }
 }
 
--- front/src/components/NotFound.js
+++ front/src/components/NotFound.js
@@ -2,19 +2,23 @@ import React from 'react';
 
 const NotFound = () => {
     return(
-        <div className="container error">
-            <div className="row">
-                <div className="col s12">
-                    <h1 className="errorTxt center pink-text text-lighten-3">404</h1>
-                </div> 
-            </div>
-            <div className="row">
-                <div className="col s12">
-                    <h1 className="errorTxt center pink-text text-lighten-3">Page Not Found</h1>
-                </div> 
-            </div>
-            <div className="row">
-                
+        <div class="valign-wrapper" style={{"width":"100%", "height":"50%", "position": "absolute"}}>
+            <div className="valign" style={{"width":"100%"}}>
+                <div className="container error">
+                    <div className="row">
+                        <div className="col s12">
+                            <h1 className="errorTxt center pink-text text-lighten-3">404</h1>
+                        </div> 
+                    </div>
+                    <div className="row">
+                        <div className="col s12">
+                            <h1 className="errorTxt center pink-text text-lighten-3">Page Not Found</h1>
+                        </div> 
+                    </div>
+                    <div className="row">
+                        
+                    </div>
+                </div>
             </div>
         </div>
     )
